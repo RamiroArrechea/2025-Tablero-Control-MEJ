d97a63562c2561e5a913fd8d7502994e0c7aea7a
Ramiro Arrechea <ramirog.arrechea@gmail.com>
2025-02-23 02:13:38 -0300

    mejoramos la funcionalidad, separando las querys y agrgamos la funcion cumpleaños

--- conexion/conexionDB.php
+++ conexion/conexionDB.php
@@ -1,10 +1,18 @@
 <?php
 
+	//	 PARA TRABAJAR EN LOCAL
 	$dbhost = "localhost";
 	$dbuser = "root";
 	$dbpass = "";
 	$dbname = "mej_baseDatos";
 	$conn ="";
+
+	//	 PARA TRABAJAR EN EL REPOSITORIO
+	/*$dbhost = "sql111.infinityfree.com";
+	$dbuser = "if0_38260221";
+	$dbpass = "f1I27xIxWYTPh";
+	$dbname = "if0_38260221_mej_baseDatos";
+	$conn ="";*/
 
 	try{
 		$conn = new mysqli($dbhost,$dbuser,$dbpass,$dbname);
